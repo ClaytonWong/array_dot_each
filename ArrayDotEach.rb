f41e74239f16d1a90948bc14073c9dd6f03f4d5c
ClaytonWong <c_w_serious-github@yahoo.com.au>
2017-09-16 21:23:29 +1000

    Add 1 file
    Add ArrayDotEach.rb

---
+++ ArrayDotEach.rb
@@ -0,0 +1,14 @@
+num_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#.each loop version 1
+num_array.each do |x|
+  puts x
+end
+
+#Print blank line between output from .each
+#loops
+puts ""
+
+#.each loop version 2
+num_array.each { |x| puts x }
+
